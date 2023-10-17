d283f5cc7987ed22f2c05424c4800375b95d112b
aisavvytoday <muhuri.json@gmail.com>
2023-10-17 06:46:21 +0300

    removed react app

--- react-app/src/components/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders App component', () => {
-  render(<App />);
-  expect(screen.getByText(/learn react/i)).toBeInTheDocument();
-});
